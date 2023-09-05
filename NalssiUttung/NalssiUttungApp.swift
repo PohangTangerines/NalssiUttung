3c2d7c59910e312f2f82608a94667c564ab27c3e
220v-K <jwlee010222@gmail.com>
2023-09-05 12:02:30 +0900

    proj init

---
+++ NalssiUttung/NalssiUttungApp.swift
@@ -0,0 +1,17 @@
+//
+//  NalssiUttungApp.swift
+//  NalssiUttung
+//
+//  Created by 이재원 on 2023/09/05.
+//
+
+import SwiftUI
+
+@main
+struct NalssiUttungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
