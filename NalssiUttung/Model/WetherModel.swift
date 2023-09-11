cced2fda09bb6f57506fbf2f6691826d565817a2
ChangJin-Lee <ckdwls525@gmail.com>
2023-09-12 00:53:49 +0900

    feature: create WeatherModel

---
+++ NalssiUttung/Model/WetherModel.swift
@@ -0,0 +1,15 @@
+//
+//  WetherModel.swift
+//  NalssiUttung
+//
+//  Created by CHANG JIN LEE on 2023/09/12.
+//
+
+import Foundation
+
+enum Weather: String{
+    case location = "제주시 아라동"
+    case temperature  = "24"
+    case high = "33"
+    case low = "21"
+}
