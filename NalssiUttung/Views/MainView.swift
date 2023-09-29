8dd676320910cc35bd9a913e0e5a34ca79a00cc2
220v-K <jwlee010222@gmail.com>
2023-09-29 18:09:04 +0900

    🪡 [Feat, Rename] IMHyemin 폰트 파일명 변경 및 메인View 작업 wip~.

--- NalssiUttung/Views/MainView.swift
+++ NalssiUttung/Views/MainView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            MainHeader()
+            Spacer()
+            
+            Text("24")
+                .font(.IMHyemin(.largeTitle))
+        }.padding(.horizontal, 15)
+            .background(Color.seaSky)
+    }
+}
+
+struct MainHeader: View {
+    let locationText: String = "제주시 애월읍"
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                Text("\(locationText)")
+                    .font(.pretendardSemibold(.callout))
+                Image(systemName: "location.fill")
+                    .font(.system(size: 16, weight: .bold))
+            }
+            HStack {
+                Spacer()
+                Image(systemName: "plus")
+                    .font(.pretendardSemibold(.body))
+            }
+        }.padding(.top, 7.5).padding(.bottom, 12)
+            .background(Color.white)
     }
 }
 
