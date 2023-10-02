04cb98ec8a6fd2afd8d6e85a352addcec0df9c42
220v-K <jwlee010222@gmail.com>
2023-10-02 21:20:28 +0900

    ✨ [Feat/#28] View Extension 추가(lineheight), Main View 작업 ing₩

--- NalssiUttung/Views/MainView.swift
+++ NalssiUttung/Views/MainView.swift
@@ -9,14 +9,44 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             MainHeader()
-            Spacer()
             
-            Text("24")
+            tempConditionRow
+                .padding(.bottom, 10)
+            
+            HStack {
+                Text("최고 \(33)° | 최저 \(24)°")
+                    .font(.pretendardMedium(.body))
+                Spacer()
+            }.padding(.bottom, 18)
+            
+            HStack {
+                Text("일교차 크난\n고뿔 들리지 않게\n조심합서!")
+                    .font(.IMHyemin(.title))
+                    .IMHyeminLineHeight(.title, lineHeight: 40)
+                Spacer()
+            }
+            
+            Spacer()
+        }.background(Color.seaSky).padding(.horizontal, 15)
+            .background(Color.pink)
+    }
+    
+    private var tempConditionRow: some View {
+        HStack(alignment: .bottom) {
+            Text("24 ")
+                .font(.IMHyemin(.largeTitle2))
+                .tracking(-(Font.DEFontSize.largeTitle.rawValue * 0.1))
+            Text("°")
                 .font(.IMHyemin(.largeTitle))
-        }.padding(.horizontal, 15)
-            .background(Color.seaSky)
+                .padding(.leading, -(Font.DEFontSize.largeTitle2.rawValue * 0.5))
+            Text("맑음")
+                .font(.IMHyemin(.title))
+                .padding(.bottom, 13)
+                .padding(.leading, -30)
+            Spacer()
+        }
     }
 }
 
