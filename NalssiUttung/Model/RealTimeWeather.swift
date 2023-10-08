f8427a385c3895488021d4e0d202f33f19a2b465
ChangJin-Lee <ckdwls525@gmail.com>
2023-10-08 15:43:13 +0900

    ✨ [feat/#29] cardview를 위한 샘플데이터 추가

--- NalssiUttung/Model/RealTimeWeather.swift
+++ NalssiUttung/Model/RealTimeWeather.swift
@@ -19,4 +19,9 @@ extension RealTimeWeather {
     static let sampleData: [RealTimeWeather] = [
         RealTimeWeather(condition: "맑음", temperature: 24, lowTemperature: 24, highTemperature: 33, weatherMessage: "일교차 크난\n고뿔 들리지 않게\n조심합서!")
     ]
+    static let sampleCardData: [RealTimeWeather] = [
+        RealTimeWeather(condition: "맑음", temperature: 24, lowTemperature: 24, highTemperature: 33, weatherMessage: "일교차 크난\n고뿔 들리지 않게\n조심합서!"),
+        RealTimeWeather(condition: "맑음", temperature: 24, lowTemperature: 24, highTemperature: 33, weatherMessage: "일교차 크난\n고뿔 들리지 않게\n조심합서!"),
+        RealTimeWeather(condition: "맑음", temperature: 24, lowTemperature: 24, highTemperature: 33, weatherMessage: "일교차 크난\n고뿔 들리지 않게\n조심합서!")
+    ]
 }
