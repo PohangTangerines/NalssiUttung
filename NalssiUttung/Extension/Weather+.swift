9ab58684ee1b1356b895223bf73b2697d304288e
220v-K <jwlee010222@gmail.com>
2023-10-10 06:47:02 +0900

    [Chore/#31] WeatherCondition Icon returning function 작업중 중단

---
+++ NalssiUttung/Extension/Weather+.swift
@@ -0,0 +1,84 @@
+//
+//  Weather+.swift
+//  NalssiUttung
+//
+//  Created by 이재원 on 2023/10/10.
+//
+
+import SwiftUI
+import WeatherKit
+//
+//extension WeatherCondition {
+//    // weatherCondition -> 이미지 에셋 String.
+//    func weatherIcon() -> String {
+//        var res = ""
+//        
+//        switch self {
+////        case .blizzard:
+////            return "halla"
+////        case .blowingDust:
+////            <#code#>
+//        case .blowingSnow, .snow, .flurries, .sunFlurries:
+//            res = "snow"
+//        case .breezy:
+//            <#code#>
+//        case .clear:
+//            res = "dayClear"
+//        case .cloudy, .mostlyCloudy:
+//            res = "cloudy"
+//        case .drizzle:
+//            res = "rainy"
+//        case .foggy:
+//            res = "haze"
+//        case .freezingDrizzle:
+//            <#code#>
+//        case .freezingRain:
+//            <#code#>
+//        case .frigid:
+//            <#code#>
+//        case .hail:
+//            <#code#>
+//        case .haze:
+//            <#code#>
+//        case .heavyRain:
+//            <#code#>
+//        case .heavySnow:
+//            <#code#>
+//        case .hot:
+//            <#code#>
+//        case .hurricane:
+//            <#code#>
+//        case .isolatedThunderstorms:
+//            res = "
+//        case .mostlyClear:
+//            res = "dayClearThenCloudy"
+//        case .partlyCloudy:
+//            res = "dayCloudyThenClear"
+//        case .rain:
+//            <#code#>
+//        case .scatteredThunderstorms:
+//            <#code#>
+//        case .sleet:
+//            <#code#>
+//        case .smoky:
+//            <#code#>
+//        case .strongStorms:
+//            <#code#>
+//        case .sunFlurries:
+//            <#code#>
+//        case .sunShowers:
+//            <#code#>
+//        case .thunderstorms:
+//            <#code#>
+//        case .tropicalStorm:
+//            <#code#>
+//        case .windy:
+//            <#code#>
+//        case .wintryMix:
+//            <#code#>
+//        default:
+//            res = "halla"
+//        }
+//        return res
+//    }
+//}
