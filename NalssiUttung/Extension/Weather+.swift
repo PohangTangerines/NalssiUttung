9ec8532b0263ab17cf19e4e4d1cc3221e74a96ec
금가경 <gageum0@gmail.com>
2023-11-07 12:21:25 +0900

    [Fix] 분기처리 수정

--- NalssiUttung/Extension/Weather+.swift
+++ NalssiUttung/Extension/Weather+.swift
@@ -92,36 +92,35 @@ extension WeatherCondition {
     
     func weatherComment() -> String {
         switch self {
-        case .clear, .mostlyClear :
+        case .clear, .mostlyClear, .hot :
             return ["볕이 과랑과랑허니 선크림 바르고 다닙서", "날씨 촘말로 좋쿠다. 기지않?", "떠납서 둘이영 몬딱 훌훌 버렴",
                     "더웡주거지켜, 수영하래 조끄뜨레 가잰?"].randomElement()!
         case .cloudy :
             return "하늘이 왁왁해졈시니 비 오려는지도 모르겠쿠다"
-        case .partlyCloudy :
+        case .partlyCloudy, .mostlyCloudy :
             return "날씨가 산도록 허니 국이 맨도롱 홀 때 호로록 들여 싸붑서"
         case .haze :
             return "안개 끼었덴허는데 조심합서"
-        case .foggy :
+        case .foggy, .blowingDust, .smoky:
             return "육지 사람들 안개가 이추룩 심한데도 무사 돌아댕기는거?"
         case .windy, .breezy :
             return ["바람 강하니 촐람생이처럼 바당 가지 말앙 집에나 있읍서", "바람 강하멘 재개재개 집으로 갑서"].randomElement()!
         
-        case .strongStorms, .scatteredThunderstorms, .isolatedThunderstorms, .thunderstorms, .tropicalStorm :
+        case .strongStorms, .scatteredThunderstorms, .isolatedThunderstorms, .thunderstorms, .tropicalStorm, .hurricane :
             return "아고게! 벼락털어졈신가?"
         case .rain :
             return "비가 졸락졸락 내렴쪄 우산 챙기쿠라"
         case .heavyRain :
             return ["비가 자락자락 내렴쪄 대맹이에 구멍나겠쿠다", "비가 와싹 오는디 괜히 바당같은데 가지마쿠라", "한라산 꼭대기까정 폭삭 젖었수다"].randomElement()!
-        case .drizzle, .freezingDrizzle :
+        case .drizzle, .freezingDrizzle, .sunShowers :
             return "비가 자락자락 내렴쪄 아멩해도 무신일 이신거 닮아"
-        case .snow :
+        case .snow , .hail, .frigid:
             return "눈 온다 고라신디 겅하면 돌하르방 대맹이에 눈 하영 쌓이겠지게"
         case .heavySnow, .blizzard, .blowingSnow, .flurries , .sunFlurries:
             return "눈이 제라 와신디 한라산 대맹이가 새하얘지쿠다"
         case .freezingRain, .sleet, .wintryMix :
             return "겨울이여부난 입김이 막 나와부신디"
             
-        // .blowingDust, .frigid, .hail, .hot, .hurricane, .mostlyCloudy, .smoky, .sunShowers
         default :
             return "기본 멘트"
         }
@@ -132,6 +131,7 @@ extension WeatherCondition {
         let sunsetDate = weatherData.weather.dailyForecast.forecast.first!.sun.sunset!
 
         switch self {
+<<<<<<< Updated upstream
         case .clear, .mostlyClear :
             if sunriseDate.timeIntervalSinceNow < 0 && sunsetDate.timeIntervalSinceNow > 0 {
                 return "clearCharacter"
@@ -147,21 +147,29 @@ extension WeatherCondition {
                 return "partlyCloudyNightCharacter"
             }
         case .haze, .foggy :
+=======
+        case .clear, .mostlyClear, .hot :
+            return "clearCharacter"
+        case .cloudy :
+            return "cloudyCharacter"
+        case .partlyCloudy, .mostlyCloudy :
+            return "partlyCloudyCharacter"
+        case .haze, .foggy, .blowingDust, .smoky :
+>>>>>>> Stashed changes
             return "foggyCharacter"
         case .windy, .breezy :
             return "windyCharacter"
-        case .strongStorms, .scatteredThunderstorms, .isolatedThunderstorms, .thunderstorms, .tropicalStorm :
+        case .strongStorms, .scatteredThunderstorms, .isolatedThunderstorms, .thunderstorms, .tropicalStorm, .hurricane :
             return "thunderstormCharacter"
-        case .rain, .drizzle, .freezingDrizzle :
+        case .rain, .drizzle, .freezingDrizzle, .sunShowers :
             return "rainCharacter"
         case .heavyRain :
             return "heavyRainCharacter"
-        case .snow, .heavySnow, .blizzard, .blowingSnow, .flurries , .sunFlurries:
+        case .snow, .heavySnow, .blizzard, .blowingSnow, .flurries , .sunFlurries, .frigid, .hail:
             return "snowCharacter"
         case .freezingRain, .sleet, .wintryMix :
             return "freezingRainCharacter"
         
-        // .blowingDust, .frigid, .hail, .hot, .hurricane, .mostlyCloudy, .smoky, .sunShowers
         default :
             return "clearCharacter"
         }
