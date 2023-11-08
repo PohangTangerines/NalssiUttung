5d813fb969aee2cb1df9381da5fea1855ccba8cf
금가경 <gageum0@gmail.com>
2023-11-08 23:41:02 +0900

    [Add] Gif 파일 추가 및 불필요한 파일 제거

---
+++ NalssiUttung/Views/AnimatedGifView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedGifView.swift
+//  NalssiUttung
+//
+//  Created by 금가경 on 11/8/23.
+//
+
+import SwiftUI
+
+struct AnimatedGifView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedGifView()
+}
