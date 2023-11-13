a91f749c37cbaa4aed969f4595cdf2a10c1f8d46
ChangJin-Lee <ckdwls525@gmail.com>
2023-11-13 14:26:49 +0900

    fix: launch Screen

--- NalssiUttungApp.swift
+++ NalssiUttungApp.swift
@@ -15,21 +15,21 @@ struct NalssiUttungApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ZStack {
+//            ZStack {
                 MainView(showSplash: $showSplash)
-                if showSplash{
-                    SplashScreenView() // 스플래시 뷰
-                        .opacity(splashOpacity)
-                        .onAppear {
-                            // 스플래시 화면이 표시된 후 일정 시간이 지나면 메인 화면으로 이동
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 2.8) {
-                                withAnimation {
-                                    splashOpacity = 0.0 // 페이드 아웃 효과를 위해 opacity를 0.0으로 변경
-                                }
-                            }
-                        }
-                }
-            }
+//                if showSplash{
+//                    SplashScreenView() // 스플래시 뷰
+//                        .opacity(splashOpacity)
+//                        .onAppear {
+//                            // 스플래시 화면이 표시된 후 일정 시간이 지나면 메인 화면으로 이동
+//                            DispatchQueue.main.asyncAfter(deadline: .now() + 2.8) {
+//                                withAnimation {
+//                                    splashOpacity = 0.0 // 페이드 아웃 효과를 위해 opacity를 0.0으로 변경
+//                                }
+//                            }
+//                        }
+//                }
+//            }
         }
     }
 }
