a51721a357e8759f6cc4785833998ee6358b5fb0
금가경 <gageum0@gmail.com>
2024-11-05 00:58:31 +0900

    [Delete] 필요 없는 주석 삭제

--- NalssiUttung/ViewModels/MainViewModel.swift
+++ NalssiUttung/ViewModels/MainViewModel.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 class MainViewModel: ObservableObject {
-//    // MARK: UI 관련 Property
-//    @Published var gifName: String = "cl"
     // MARK: View 전환, Gesture 관련 Property
     @Published var displayedContent: DisplayedContent = .main
     @Published var dragOffset: CGSize = .zero
