0875b31420acef241c41955cfa16cf4a5b22e344
금가경 <gageum0@gmail.com>
2024-11-12 13:23:53 +0900

    [Fix] Gesture 있을 때마다 배경 날아가는 문제 해결

--- NalssiUttung/Views/MainView/MainView.swift
+++ NalssiUttung/Views/MainView/MainView.swift
@@ -13,11 +13,10 @@ struct MainView: View {
     @StateObject var viewModel = MainViewModel()
     @ObservedObject var locationManager = LocationManager.shared
     @StateObject var weatherManager = WeatherManager()
-    @StateObject var localizedWeatherViewModel = LocalizedWeatherViewModel()
     
     let mode: WeatherDisplayMode
     let viewOrigin: ViewOrigin
-
+    
     private var dragGesture: some Gesture {
         DragGesture()
             .onChanged { gesture in
@@ -33,23 +32,25 @@ struct MainView: View {
             ZStack {
                 Color.seaSky
                     .ignoresSafeArea()
-                switch viewModel.displayedContent {
-                    
-                case .main:
-                    CurrentWeatherView(weatherManager: weatherManager, viewModel: viewModel)
-                        .transition(.move(edge: .top))
-                        .padding(.horizontal, 20)
-                    
-                case .detail:
-                    VStack {
-                        CurrentWeatherDetailView(weatherManager: weatherManager)
-                            .transition(.move(edge: .bottom))
+                Group {
+                    switch viewModel.displayedContent {
+                        
+                    case .main:
+                        CurrentWeatherView(weatherManager: weatherManager, viewModel: viewModel)
+                            .transition(.move(edge: .top))
                             .padding(.horizontal, 20)
+                        
+                    case .detail:
+                        VStack {
+                            CurrentWeatherDetailView(weatherManager: weatherManager)
+                                .transition(.move(edge: .bottom))
+                                .padding(.horizontal, 20)
+                        }
                     }
                 }
+                .gesture(dragGesture)
+                .offset(y: viewModel.viewOffsetY)
             }
-            .gesture(dragGesture)
-            .offset(y: viewModel.viewOffsetY)
             .toolbar(content: toolbarContent)
             .task {
                 switch viewOrigin {
